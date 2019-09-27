6c86e98c0fba12f05c0db527b27c309ab0eed7fb
Sagardeep Singh <sagardeep.0019@gmail.com>
2019-09-26 19:12:43 -0700

    Added a route for password reset page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { compose } from "recompose";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Navigation from "../Navigation";
 import LandingPage from "../Landing";
 import SignUpPage from "../SignUp";
@@ -8,19 +10,26 @@ import PasswordForgetPage from "../PasswordForget";
 import HomePage from "../Home";
 import AccountPage from "../Account";
 import AdminPage from "../Admin";
+
 import * as ROUTES from "../../constants/routes";
+import { withAuthentication } from "../Session";
 
-const App = () => (
-    <Router>
-        <Navigation />
-        <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-        <Route path={ROUTES.HOME} component={HomePage} />
-        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </Router>
-);
+const App = () => {
+    return (
+        <Router>
+            <Navigation />
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+            <Route
+                path={ROUTES.PASSWORD_FORGET}
+                component={PasswordForgetPage}
+            />
+            <Route path={ROUTES.HOME} component={HomePage} />
+            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+            <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </Router>
+    );
+};
 
-export default App;
+export default compose(withAuthentication)(App);
